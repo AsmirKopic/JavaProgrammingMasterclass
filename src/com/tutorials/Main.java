034f2a5e46331334f7f81f08430f7312b176f3db
Asmir Kopic <asmir.kopic123@gmail.com>
2021-05-25 22:17:26 +0200

    First commit.

--- src/com/tutorials/Main.java
+++ src/com/tutorials/Main.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" test");
 
+        // Test
+        Hamburger hamburger = new Hamburger("Basic", "Sausane", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Cheese", 3.55);
+        hamburger.addHamburgerAddition2("Cheese", 2.35);
 
     }
 }
