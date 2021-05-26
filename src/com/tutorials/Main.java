ae47e18d60aa381c0a873c3eab6b8021557bbcc5
Asmir Kopic <asmir.kopic123@gmail.com>
2021-05-26 19:55:29 +0200

    commit

--- src/com/tutorials/Main.java
+++ src/com/tutorials/Main.java
@@ -10,7 +10,8 @@ public class Main {
         Hamburger hamburger = new Hamburger("Basic", "Sausane", 3.56, "White");
         double price = hamburger.itemizeHamburger();
         hamburger.addHamburgerAddition1("Cheese", 3.55);
-        hamburger.addHamburgerAddition2("Cheese", 2.35);
-
+        hamburger.addHamburgerAddition2("Tomato", 2.35);
+        hamburger.addHamburgerAddition3(" Random", 4.67);
+        hamburger.itemizeHamburger();
     }
 }
