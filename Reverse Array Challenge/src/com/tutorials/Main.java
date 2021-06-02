82b82ee77376dc71c98739a09563794e1a0c3493
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-02 21:33:05 +0200

    Add files via upload

---
+++ Reverse Array Challenge/src/com/tutorials/Main.java
@@ -0,0 +1,54 @@
+package com.tutorials;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+        int[] myArray = readIntegers(8);
+
+
+        System.out.println(Arrays.toString(myArray));
+
+        System.out.println(" Minimum number in array is " + findMin(myArray));
+
+        //Call reverse method
+        reverse(myArray);
+        reverse(myArray);
+
+
+    }
+
+    private static int[] readIntegers(int size){
+        System.out.println("Please enter " + size + " numbers" );
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = input.nextInt();
+        }
+        return array;
+    }
+
+    private static int findMin(int[] array){
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    private static void reverse(int[] array){
+        int[] reverseArray = new int[array.length];
+        for (int i = 0, j = array.length-1; i < array.length; i++,j--) {
+            reverseArray[j] = array[i];
+        }
+        System.out.println(Arrays.toString(reverseArray));
+    }
+}
