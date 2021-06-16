2fd8b71b7e7085deea5df09c93e368326be04538
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-16 05:09:29 +0200

    Delete Section 10 Inner and Abstract classes & Interfaces/Interfaces/src/com/tutorials directory

--- Section 10 Inner and Abstract classes & Interfaces/Interfaces/src/com/tutorials/ITelephone.java
+++
@@ -1,9 +0,0 @@
-package com.tutorials;
-
-public interface ITelephone {
-    void powerOn();
-    void dial(int phoneNumber);
-    boolean isRinging();
-    void answer();
-    boolean callPhone(int phoneNumber);
-}
