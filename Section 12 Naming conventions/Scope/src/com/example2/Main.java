e6c58ebbd51724b281fbcab3ec3b93adc137447a
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-25 23:42:48 +0200

    scope check methods

---
+++ Section 12 Naming conventions/Scope/src/com/example2/Main.java
@@ -0,0 +1,24 @@
+package com.example2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String privateVar = "this is private to main()";
+
+        ScopeCheck scopeInstance = new ScopeCheck();
+
+        System.out.println("Scope instance privateVar is " + scopeInstance.getPrivateVar());
+        System.out.println(privateVar);
+
+        scopeInstance.timesTwo();
+
+        System.out.println("==============================");
+        ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+
+        innerClass.timesTwo();
+
+
+
+
+    }
+}
