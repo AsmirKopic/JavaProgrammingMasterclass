196ce8b61c3055a2b279eb43fead4a6e39a463b2
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-28 06:10:28 +0200

    Accessible interface added

---
+++ Section 12 Naming conventions/AccessModifiers/src/com/tutorials/Accesible.java
@@ -0,0 +1,11 @@
+package com.tutorials;
+
+public interface Accesible {
+
+    // Challenge text ..
+    int SOME_CONSTANT = 100;
+    public void methodA();
+    void methodB();
+    boolean methodC();
+
+}
