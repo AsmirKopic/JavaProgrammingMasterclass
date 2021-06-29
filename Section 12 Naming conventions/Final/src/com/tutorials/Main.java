1c65586734fbd4fb70e78c37c9628b109e6f1cf4
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-29 06:20:19 +0200

    Password class

--- Section 12 Naming conventions/Final/src/com/tutorials/Main.java
+++ Section 12 Naming conventions/Final/src/com/tutorials/Main.java
@@ -3,6 +3,23 @@ package com.tutorials;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+       int pw = 674321;
+       Password password = new Password(pw);
+       password.storePassword();
+
+       password.letMeIn(1234);
+       password.letMeIn(3634636);
+       password.letMeIn(124134);
+       password.letMeIn(674321);
+
+
+
+
+
+
+
+
+
     }
 }
