3bae01b07b89365ccfda3753adb62aca942d2e13
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-29 05:20:11 +0200

    static instances

--- Section 12 Naming conventions/Static/src/com/tutorials/Main.java
+++ Section 12 Naming conventions/Static/src/com/tutorials/Main.java
@@ -3,6 +3,15 @@ package com.tutorials;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	StaticTest firstInstance = new StaticTest("1st Instance");
+    System.out.println(firstInstance.getName() + " is instance number " + firstInstance.getNumInstance());
+
+    StaticTest secondInstance = new StaticTest("2nd Instance");
+    System.out.println(secondInstance.getName() + " is instance number " + secondInstance.getNumInstance());
+
+    StaticTest thirdInstance = new StaticTest("3rd Instance");
+    System.out.println(secondInstance.getName() + " is instance number " + thirdInstance.getNumInstance());
+
+
     }
 }
