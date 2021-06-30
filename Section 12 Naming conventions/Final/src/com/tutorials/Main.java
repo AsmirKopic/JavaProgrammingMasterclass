03eb79bbcc41d4c9d12ccea030749c2cabba9feb
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-30 05:38:11 +0200

    Final statement

--- Section 12 Naming conventions/Final/src/com/tutorials/Main.java
+++ Section 12 Naming conventions/Final/src/com/tutorials/Main.java
@@ -4,16 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-       int pw = 674321;
-       Password password = new Password(pw);
-       password.storePassword();
+//       int pw = 674321;
+//       Password password = new Password(pw);
+//       password.storePassword();
+//
+//       password.letMeIn(1234);
+//       password.letMeIn(3634636);
+//       password.letMeIn(124134);
+//       password.letMeIn(674321);
 
-       password.letMeIn(1234);
-       password.letMeIn(3634636);
-       password.letMeIn(124134);
-       password.letMeIn(674321);
+        SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
 
-
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
 
 
 
