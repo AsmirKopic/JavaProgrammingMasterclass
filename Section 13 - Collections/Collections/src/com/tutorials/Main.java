54e1dfc74962f0f0b95a6c91c5b38d1688efb54e
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-06-30 06:07:13 +0200

    Collections

---
+++ Section 13 - Collections/Collections/src/com/tutorials/Main.java
@@ -0,0 +1,14 @@
+package com.tutorials;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int test = 'B';
+
+        System.out.println(test);
+        System.out.println('B');
+
+
+    }
+}
