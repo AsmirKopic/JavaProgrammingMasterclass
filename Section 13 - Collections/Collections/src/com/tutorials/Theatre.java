4285d868c1767b7b7c8a3e8d01049d39b61dc949
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-07-01 05:34:01 +0200

    Theatre add Seat, reserve and cancel reservation method

--- Section 13 - Collections/Collections/src/com/tutorials/Theatre.java
+++ Section 13 - Collections/Collections/src/com/tutorials/Theatre.java
@@ -3,6 +3,7 @@ package com.tutorials;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
@@ -24,6 +25,55 @@ public class Theatre {
     }
     public boolean reserveSeat(String seatNumber){
         Seat requestedSeat = null;
+        for (Seat seat : seats){
+            if (seat.getSeatNumber().equals(seatNumber)){
+                requestedSeat = seat;
+                break;
+            }
+        }
+        if (requestedSeat == null){
+            System.out.println("There is no seat " + seatNumber);
+            return false;
+        }
+        return requestedSeat.reserve();
+    }
+    // method for testing
+    public void getSeats(){
+        for (Seat seat : seats){
+            System.out.println(seat.getSeatNumber());
+        }
     }
 
+    private class Seat {
+        private final String seatNumber;
+        private boolean reserved = false;
+
+        public Seat(String seatNumber) {
+            this.seatNumber = seatNumber;
+        }
+
+        public boolean reserve(){
+            if (!this.reserved){
+                this.reserved = true;
+                System.out.println("Seat " + seatNumber + " reserved.");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean cancel() {
+            if (this.reserved){
+                this.reserved = false;
+                System.out.println("Reservation of seat " + seatNumber + " is canceled.");
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public String getSeatNumber() {
+            return seatNumber;
+        }
+    }
 }
