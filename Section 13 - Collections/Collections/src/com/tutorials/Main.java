0148848b38564eb5e6c4e9d8731638b889f60da5
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-07-02 23:00:08 +0200

    testing methods

--- Section 13 - Collections/Collections/src/com/tutorials/Main.java
+++ Section 13 - Collections/Collections/src/com/tutorials/Main.java
@@ -1,22 +1,27 @@
 package com.tutorials;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Theatre theatre = new Theatre("Olympian", 8, 12);
-        theatre.getSeats();
-        if (theatre.reserveSeat("A08")){
-            System.out.println("Please pay");
-        } else{
-            System.out.println("Seat is already taken.");
-        }
-        if (theatre.reserveSeat("B04")){
-            System.out.println("Please pay");
-        } else{
-            System.out.println("Seat is already taken.");
-        }
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
 
-
+        Collections.shuffle(seatCopy);
+        printList(seatCopy);
+        
     }
+    public static void printList(List<Theatre.Seat> list){
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("============================");
+    }
+
 }
