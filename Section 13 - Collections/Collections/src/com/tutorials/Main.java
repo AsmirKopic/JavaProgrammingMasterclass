8e75282158b65322052172955fc852b0f6969962
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-07-02 21:57:24 +0200

    Comparable implm

--- Section 13 - Collections/Collections/src/com/tutorials/Main.java
+++ Section 13 - Collections/Collections/src/com/tutorials/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
         Theatre theatre = new Theatre("Olympian", 8, 12);
         theatre.getSeats();
-        if (theatre.reserveSeat("H11")){
+        if (theatre.reserveSeat("A08")){
             System.out.println("Please pay");
         } else{
             System.out.println("Seat is already taken.");
         }
-        if (theatre.reserveSeat("H11")){
+        if (theatre.reserveSeat("B04")){
             System.out.println("Please pay");
         } else{
             System.out.println("Seat is already taken.");
--- Section 13 - Collections/Collections/src/com/tutorials/Theatre.java
+++ Section 13 - Collections/Collections/src/com/tutorials/Theatre.java
@@ -1,13 +1,10 @@
 package com.tutorials;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private Collection<Seat> seats = new LinkedList<>();
+    private List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
@@ -25,19 +22,17 @@ public class Theatre {
         return theatreName;
     }
     public boolean reserveSeat(String seatNumber){
-        Seat requestedSeat = null;
-        for (Seat seat : seats){
-            if (seat.getSeatNumber().equals(seatNumber)){
-                requestedSeat = seat;
-                break;
-            }
-        }
-        if (requestedSeat == null){
-            System.out.println("There is no seat " + seatNumber);
-            return false;
-        }
-        return requestedSeat.reserve();
+        Seat requestedSeat = new Seat(seatNumber);
+       int foundSeat = Collections.binarySearch(seats,requestedSeat,null);
+       if (foundSeat >= 0){
+           return seats.get(foundSeat).reserve();
+       } else {
+           System.out.println("There is no seat " + seatNumber);
+           return false;
+       }
     }
+
+
     // method for testing
     public void getSeats(){
         for (Seat seat : seats){
@@ -46,6 +41,7 @@ public class Theatre {
     }
 
     private class Seat implements Comparable<Seat> {
+
         private final String seatNumber;
         private boolean reserved = false;
 
