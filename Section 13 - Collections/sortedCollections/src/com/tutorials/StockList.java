fd27ff94eff5eb4287604286381a654ed6f5ce22
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-07-21 05:12:53 +0200

    addStock method

--- Section 13 - Collections/sortedCollections/src/com/tutorials/StockList.java
+++ Section 13 - Collections/sortedCollections/src/com/tutorials/StockList.java
@@ -4,9 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StockList {
-    private final Map<String, StockItems> list;
+    private final Map<String, StockItem> list;
 
     public StockList() {
         this.list = new HashMap<>();
+    }
+
+    public int addStock(StockItem item){
+        if (item != null){
+            // check if already have quantities in stock
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // if there are already stocks on this item, adjust the quantity
+            if (inStock != item){
+                item.adjustStock(inStock.getQuantityStock());
+            }
+            list.put(item.getName(), item);
+            return item.getQuantityStock();
+        }
+        return 0;
     }
 }
