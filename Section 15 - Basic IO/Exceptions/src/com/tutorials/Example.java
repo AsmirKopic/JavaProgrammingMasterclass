67a65c9c574028d050b68d58f5b8327925493463
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-07-26 06:24:29 +0200

    example class

---
+++ Section 15 - Basic IO/Exceptions/src/com/tutorials/Example.java
@@ -0,0 +1,30 @@
+package com.tutorials;
+
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+
+    }
+    private static int divide(){
+        int x = getInt();
+        int y = getInt();
+        System.out.println("X is " + x + " ,Y is " + y);
+        return x / y;
+    }
+
+    private static int getInt(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter and integer. ");
+        return s.nextInt();
+    }
+
+
+
+
+
+
+}
+
--- Section 15 - Basic IO/Exceptions/src/com/tutorials/Main.java
+++ Section 15 - Basic IO/Exceptions/src/com/tutorials/Main.java
@@ -1,8 +1,47 @@
 package com.tutorials;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//	int x = 98;
+//	int y = 0;
+//        System.out.println(devideLBYL(x,y));
+//        System.out.println(devideEAFP(x, y));
+
+        int x = getInt();
+        System.out.println("x is " + x);
+
     }
+    private static int getInt(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        try {
+            return s.nextInt();
+        } catch (InputMismatchException e){
+            return 0;
+        }
+    }
+
+    private static int devideLBYL(int x, int y){
+        if (y != 0){
+            return x / y;
+        } else {
+            return 0;
+        }
+    }
+
+    private static int devideEAFP(int x, int y){
+        try {
+            return x / y;
+        } catch (ArithmeticException e){
+            // System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+
+
 }
