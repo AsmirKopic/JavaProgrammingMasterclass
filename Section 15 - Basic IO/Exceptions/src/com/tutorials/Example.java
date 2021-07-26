c736c65b246821c8bc165449f9f45025ca90f376
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-07-26 22:50:05 +0200

    exceptions intro

--- Section 15 - Basic IO/Exceptions/src/com/tutorials/Example.java
+++ Section 15 - Basic IO/Exceptions/src/com/tutorials/Example.java
@@ -1,30 +1,49 @@
 package com.tutorials;
 
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+     try {
+         int result = divide();
+         System.out.println(result);
+     } catch (ArithmeticException e){
+         System.out.println(e.toString());
+         System.out.println("Unable to perform division.");
+     }
 
     }
     private static int divide(){
-        int x = getInt();
-        int y = getInt();
-        System.out.println("X is " + x + " ,Y is " + y);
-        return x / y;
+        int x;
+        int y;
+        try{
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x + " y is " + y);
+            return x / y;
+        } catch (NoSuchElementException e){
+            throw new ArithmeticException("No suitable input.");
+        } catch (ArithmeticException e){
+            throw new ArithmeticException("Attempt to divide by zero.");
+        }
     }
 
     private static int getInt(){
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter and integer. ");
-        return s.nextInt();
+        while(true){
+            try{
+                return s.nextInt();
+            } catch (InputMismatchException e){
+                // go round again
+                s.nextLine();
+                System.out.println("Please enter number using only digits 0 to 9");
+            }
+        }
     }
-
-
-
-
-
 
 }
 
--- Section 15 - Basic IO/Exceptions/src/com/tutorials/Main.java
+++ Section 15 - Basic IO/Exceptions/src/com/tutorials/Main.java
@@ -41,7 +41,4 @@ public class Main {
             return 0;
         }
     }
-
-
-
 }
