becbbaaf01ffbb6f6ad00802d29972877efbd9b0
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-08-21 22:12:15 +0200

    Asserts, After/Before class

--- Section 19 - Unit testing/Banking/src/com/tutorials/BankAccountTest.java
+++ Section 19 - Unit testing/Banking/src/com/tutorials/BankAccountTest.java
@@ -1,45 +1,73 @@
 package com.tutorials;
 
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+    private static int count;
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("This executes before tests. count is " + count++);
+    }
+
+    @org.junit.Before
+    public void setup(){
+        account = new BankAccount("Asmir", "Kopic", 1000, BankAccount.CHECKING);
+        System.out.println("Running a test.. count " + count++);
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Asmir", "Kopic", 1000, BankAccount.CHECKING);
         double balance = account.deposit(200, true);
         assertEquals(1200, balance, 0);
 
     }
 
     @org.junit.Test
-    public void withdraw() {
-        BankAccount account = new BankAccount("Asmir", "Kopic", 1000, BankAccount.CHECKING);
-        double balance = account.withdraw(200, true);
-        assertEquals(800, balance, 0);
+    public void withdraw_branch() throws Exception {
+        double balance = account.withdraw(600, true);
+        assertEquals(400, balance, 0);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception{
+        double balance = account.withdraw(600, false);
+        assertEquals(400, balance, 0);
     }
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Asmir", "Kopic", 1000, BankAccount.CHECKING);
         account.deposit(200, true);
         assertEquals(1200, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Asmir", "Kopic", 1000, BankAccount.CHECKING);
         account.withdraw(200, true);
         assertEquals(800, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Asmir", "Kopic", 1000, BankAccount.CHECKING);
         assertTrue(account.isChecking());
     }
 
-    // continue on vid 7
+    @org.junit.AfterClass
+    public static void afterClass(){
+        System.out.println("This executes after all tests, count is "+ count++);
+    }
+
+    @org.junit.After
+    public void teardown(){
+        System.out.println("Count "+ count++);
+    }
+
 }
 
 
