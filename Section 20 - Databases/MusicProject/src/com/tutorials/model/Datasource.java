7e69bab453aad56aba793674483eb78137fef39e
Asmir Kopić <67395100+AsmirKopic@users.noreply.github.com>
2021-08-31 23:30:18 +0200

    insertAlbum method added

--- Section 20 - Databases/MusicProject/src/com/tutorials/model/Datasource.java
+++ Section 20 - Databases/MusicProject/src/com/tutorials/model/Datasource.java
@@ -199,6 +199,31 @@ public class Datasource {
         }
     }
 
+    private int insertAlbum(String name,int artisId) throws SQLException {
+
+        // Check if artist is already in database
+        queryAlbum.setString(1, name);
+        ResultSet results = queryAlbum.executeQuery();
+        if (results.next()) {
+            return results.getInt(1);
+        } else {
+            // if album is not into database, insert album
+            insertIntoAlbums.setString(1, name);
+            insertIntoAlbums.setInt(2, artisId);
+            int affectedRows = insertIntoAlbums.executeUpdate();
+
+            if (affectedRows != 1) {
+                throw new SQLException("Couldn't insert album.");
+            }
+
+            ResultSet generatedKeys = insertIntoAlbums.getGeneratedKeys();
+            if(generatedKeys.next()){
+                return generatedKeys.getInt(1);
+            } else {
+                throw new SQLException("Couldn't get id from album");
+            }
+        }
+    }
 
 }
 
